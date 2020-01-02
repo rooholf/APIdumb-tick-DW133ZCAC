a431452aff8099aec788da0250f2b93eddd4e830
Muhammad Randi <rooholf@gmail.com>
2020-01-03 03:20:19 +0700

    wishlist table edit

--- models/wishlist.js
+++ models/wishlist.js
@@ -1,19 +1,19 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const whislist = sequelize.define('wishlist', {
+  const wishlist = sequelize.define('wishlist', {
     eventId: DataTypes.INTEGER,
     userId: DataTypes.INTEGER
   }, {});
-  whislist.associate = function(models) {
+  wishlist.associate = function(models) {
     // associations can be defined here
-    order.belongsTo(models.event, {
+    wishlist.belongsTo(models.event, {
       as: 'event',
       foreignKey: 'eventId'
     }),
-    order.belongsTo(models.user, {
+    wishlist.belongsTo(models.user, {
       as: 'user',
       foreignKey: 'userId'
     })
   };
-  return whislist;
+  return wishlist;
 };
