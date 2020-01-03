68e4cc599b00a452e7bf1a866fb27aa2aa333003
Muhammad Randi <rooholf@gmail.com>
2020-01-04 05:06:03 +0700

    addEvent

--- controllers/_post.js
+++ controllers/_post.js
@@ -1,32 +1,41 @@
 const models = require("../models");
 const user = models.user;
-const order = models.order
-const wishlist = models.wishlist
+const order = models.order;
+const wishlist = models.wishlist;
+const event = models.event;
 
 exports.storeUser = (req, res) => {
-    user.create(req.body).then(data =>
-      res.send({
-        message: "success",
-        data
-      })
-    );
-  };
-  
+  user.create(req.body).then(data =>
+    res.send({
+      message: "success",
+      data
+    })
+  ).catch(err=>res.send(err))
+};
 
-  exports.storeOrder = (req, res) => {
-    order.create(req.body).then(data =>
-      res.send({
-        message: "success",
-        data
-      })
-    );
-  };
+exports.storeEvent = (req, res) => {
+  event.create(req.body).then(data =>
+    res.send({
+      message: "success",
+      data
+    })
+  ).catch(err=>res.send(err))
+};
 
-  exports.storeWishlist = (req, res) => {
-    wishlist.create(req.body).then(data =>
-      res.send({
-        message: "success",
-        data
-      })
-    );
-  };
+exports.storeOrder = (req, res) => {
+  order.create(req.body).then(data =>
+    res.send({
+      message: "success",
+      data
+    })
+  ).catch(err=>res.send(err))
+};
+
+exports.storeWishlist = (req, res) => {
+  wishlist.create(req.body).then(data =>
+    res.send({
+      message: "success",
+      data
+    })
+  ).catch(err=>res.send(err))
+};
--- index.js
+++ index.js
@@ -1,64 +1,61 @@
 require("express-group-routes");
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 const showController = require("./controllers/_show");
 const deleteController = require("./controllers/_delete");
 const postController = require("./controllers/_post");
 const updateController = require("./controllers/_update");
-const authController = require("./controllers/_auth")
+const authController = require("./controllers/_auth");
 
 app.use(bodyParser.json());
 
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 
-app.get("/",(req,res)=>{
-    res.send("hello world")
-})
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
 app.group("/api", router => {
-    router.get("/",(req,res)=>{
-        res.send("hello world")
-    })
+  router.get("/", (req, res) => {
+    res.send("hello world");
+  });
 
-    // auth endpoint
-    router.post("/login", authController.login)
-    router.post("/register", authController.register)
+  // auth endpoint
+  router.post("/login", authController.login);
+  router.post("/register", authController.register);
 
-    //user endpoint
-    router.get("/users",showController.showAllUser)
-    router.post("/users", postController.storeUser)
-    router.get("/user/:id",showController.showOneUser)
-    router.patch("/user/:id", updateController.updateUser);
+  //user endpoint
+  router.get("/users", showController.showAllUser);
+  router.post("/users", postController.storeUser);
+  router.get("/user/:id", showController.showOneUser);
+  router.patch("/user/:id", updateController.updateUser);
 
-    //category endpoint
-    router.get("/categories", showController.showAllCategories)
-    router.get("/category/:id", showController.showEventOnCategory)
+  //category endpoint
+  router.get("/categories", showController.showAllCategories);
+  router.get("/category/:id", showController.showEventOnCategory);
 
+  //events endpoint
+  router.get("/events", showController.showAllEvents);
+  router.get("/event/:id", showController.showOneEvent);
+  router.post("/event", postController.storeEvent);
 
-    //events endpoint
-    router.get("/events", showController.showAllEvents)
-    router.get("/event/:id", showController.showOneEvent)
+  //order endpoint
+  router.post("/order", postController.storeOrder);
+  router.get("/order", showController.showAllOrder);
+  router.get("/order/:id", showController.showOneOrder);
+  router.patch("/order/:id", updateController.updateOrder);
 
-
-    //order endpoint
-    router.post("/order",postController.storeOrder )
-    router.get("/order", showController.showAllOrder)
-    router.get("/order/:id",showController.showOneOrder)
-    router.patch("/order/:id", updateController.updateOrder)
-
-
-    //wishlist endpoint
-    router.get("/wishlist",showController.showAllWishlist)
-    router.post("/wishlist",postController.storeWishlist)
-    router.get("/wishlist/:id",showController.showOneWishlist)
-    router.get("/wishlist/:id/user",showController.showWishlistOnUser)
-})
-
+  //wishlist endpoint
+  router.get("/wishlist", showController.showAllWishlist);
+  router.post("/wishlist", postController.storeWishlist);
+  router.get("/wishlist/:id", showController.showOneWishlist);
+  router.get("/wishlist/:id/user", showController.showWishlistOnUser);
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
